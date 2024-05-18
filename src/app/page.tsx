31e94617f933e5a27699c3d544371caea35f1fb3
jeremyn-nmq <quannguyen.jn@gmail.com>
2024-05-18 18:28:23 +0700

    Update structure

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import {useEffect, useState} from "react";
+import {BASE_API_URL} from "../../utils/constants";
+
+const Home: React.FC = () => {
+  const [parkingMap, setParkingMap] = useState([])
+
+    const handleDataRequest = () => {
+        if (!BASE_API_URL) {
+            return null;
+        }
+        fetch(`${BASE_API_URL}/api/getParkingMap`)
+            .then(response => response.json())
+            .then(data => setParkingMap(data))
+    }
+
+    return (
+        <div>
+            <h1>Home</h1>
+            <button onClick={handleDataRequest}>Get Data</button>
+            <h2> Parking Map Data: {parkingMap.slice(0,9) ?? "Not available"}</h2>
+        </div>
+    );
+};
+
+export default Home;
