a2b732f5ad11b49889a89d0021b140f83c5604b6
jeremyn-nmq <quannguyen.jn@gmail.com>
2024-05-18 16:53:54 +0700

    Update add button to fetch data

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,18 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 function index() {
     const [parkingMap, setParkingMap] = useState([])
 
-    useEffect(() => {
+    const handleDataRequest = () => {
         fetch('http://localhost:3005/api/getParkingMap')
         .then(response => response.json())
         .then(data => setParkingMap(data))
-    },[])
+    }
+
   return (
     <div>
       <h1>Home</h1>
-      <h1>{parkingMap.slice(0,9)}</h1>
+      <button onClick={handleDataRequest}>Get Data</button>
+        <h2> Parking Map Data: {parkingMap.slice(0,9) ?? "Not available"}</h2>
     </div>
   );
 }
