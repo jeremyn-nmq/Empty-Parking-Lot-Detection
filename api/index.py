fea2e2029c6bf02c6c2e9381ba10d43938a60c30
jeremyn-nmq <quannguyen.jn@gmail.com>
2024-05-19 11:46:01 +0700

    Try local 5

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask_cors import CORS
 import json
-# import gzip
-# import sys
-# sys.path.append('../python')
-# from python import image_detect
+import gzip
+import sys
+sys.path.append('../python')
+from python import image_detect
 
 app = Flask(__name__)
 CORS(app)
@@ -14,12 +14,13 @@ def get_parking_map():
     with open('./result/parking_lot_map.json', 'r') as file:
         parking_map = json.load(file)
     # parking_map = image_detect.detect_parking_spaces()
-    # content = gzip.compress(json.dumps(parking_map).encode('utf-8'), 5)
-    # response = make_response(content)
-    # response.headers['Content-length'] = len(content)
-    # response.headers['Content-Encoding'] = 'gzip'
+    content = gzip.compress(json.dumps(parking_map).encode('utf-8'), 5)
+    response = make_response(content)
+    response.headers['Content-length'] = len(content)
+    response.headers['Content-Encoding'] = 'gzip'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     # print(parking_map)
-    return json.dumps(parking_map)
+    return response
 
 
 if __name__ == '__main__':
